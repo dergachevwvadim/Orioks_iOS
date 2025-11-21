986c73da215088b1c2c239669a6223d2912e199b
Vadim <vadim.valentinovic@mail.ru>
2025-11-21 11:32:50 +0300

    Initial Commit

---
+++ Orioks-iOSTests/Orioks_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Orioks_iOSTests.swift
+//  Orioks-iOSTests
+//
+//  Created by User on 21.11.2025.
+//
+
+import Testing
+@testable import Orioks_iOS
+
+struct Orioks_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
